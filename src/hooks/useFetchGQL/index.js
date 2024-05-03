4c34cfd7b62920fb271469ca193d8aab4db31205
LAPTOP-PQCRL0O0\Utsav <utsav>
2024-05-03 17:34:39 +0530

    serial numbers is added

--- src/hooks/useFetchGQL/index.js
+++ src/hooks/useFetchGQL/index.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useMemo } from "react";
 import { useQuery } from "@apollo/client";
 export function useFetchGQL(fetchQuery, limit, filters) {
   const [page, setPage] = useState(1);
@@ -13,23 +13,34 @@ export function useFetchGQL(fetchQuery, limit, filters) {
 
   const { items, pageInfo } = queryResult?.queryResponse ?? {};
 
-  const { currentPage, itemCount } = pageInfo ?? {};
+  const { currentPage, pageCount, itemCount } = pageInfo ?? {};
 
   const nextPage = useCallback(() => {
     setPage((prevPage) => prevPage + 1);
   }, []);
 
   const resetPagination = useCallback(() => {
-    setPage(0);
+    setPage(1);
   }, []);
+
+  let updatedItems = useMemo(() => {
+    return items?.map((item, i) => {
+      let updatedItem = { ...item };
+      let curOffset = limit * (page - 1);
+      updatedItem["srNo"] = curOffset + i + 1;
+
+      return updatedItem;
+    });
+  }, [items, limit, page]);
 
   return {
     isLoading: loading,
     isError: error,
     apiResponse: {
-      data: items ?? [],
+      data: updatedItems ?? [],
       additionalInfo: {
-        totalPages: itemCount,
+        totalDocuments: itemCount,
+        totalPages: pageCount,
         currentPage,
       },
     },
